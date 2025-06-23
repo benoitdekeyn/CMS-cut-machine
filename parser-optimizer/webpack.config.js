d54ba86de6692e5e576dc6080f8938c30fbe7cbc
Benoît de Keyn <benoit.dekeyn@algosup.com>
2025-06-23 10:40:07 +0200

    new release

--- parser-optimizer/webpack.config.js
+++ parser-optimizer/webpack.config.js
@@ -2,69 +2,82 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
-module.exports = {
-  mode: process.env.NODE_ENV || 'production',
-  entry: './src/js/index.js',
-  output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist'),
-    publicPath: ''
-  },
-  resolve: {
-    alias: {
-      '@algorithms': path.resolve(__dirname, 'src/js/algorithms'),
-      '@ffd': path.resolve(__dirname, 'src/js/algorithms/First-Fit-Decreasing'),
-      '@ilp': path.resolve(__dirname, 'src/js/algorithms/Integer-Linear-Programming'),
-      '@css': path.resolve(__dirname, 'src/css')
+module.exports = (env) => {
+  // Check if we want readable output
+  const isReadableOutput = env && env.readableOutput;
+  
+  return {
+    mode: process.env.NODE_ENV || (isReadableOutput ? 'development' : 'production'),
+    entry: './src/js/index.js',
+    output: {
+      filename: 'bundle.js',
+      path: path.resolve(__dirname, 'dist'),
+      publicPath: ''
     },
-    fallback: {
-      "fs": false,
-      "child_process": false,
-      "path": false
-    }
-  },
-  module: {
-    rules: [
-      {
-        test: /\.css$/,
-        use: [MiniCssExtractPlugin.loader, 'css-loader'],
+    resolve: {
+      alias: {
+        '@algorithms': path.resolve(__dirname, 'src/js/algorithms'),
+        '@ffd': path.resolve(__dirname, 'src/js/algorithms/First-Fit-Decreasing'),
+        '@ilp': path.resolve(__dirname, 'src/js/algorithms/Integer-Linear-Programming'),
+        '@css': path.resolve(__dirname, 'src/css')
       },
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader',
-          options: {
-            presets: ['@babel/preset-env']
+      fallback: {
+        "fs": false,
+        "child_process": false,
+        "path": false
+      }
+    },
+    module: {
+      rules: [
+        {
+          test: /\.css$/,
+          use: [
+            MiniCssExtractPlugin.loader, 
+            'css-loader'
+          ],
+        },
+        {
+          test: /\.js$/,
+          exclude: /node_modules/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env']
+            }
           }
         }
-      }
-    ],
-  },
-  plugins: [
-    new MiniCssExtractPlugin({
-      filename: '[name].css',
-    }),
-    new HtmlWebpackPlugin({
-      template: './src/index.html',
-      filename: 'index.html',
-      inject: 'body',
-      minify: {
-        collapseWhitespace: true,
-        removeComments: true,
-        removeRedundantAttributes: true,
-        removeScriptTypeAttributes: true,
-        removeStyleLinkTypeAttributes: true,
-        useShortDoctype: true
-      }
-    })
-  ],
-  devServer: {
-    static: {
-      directory: path.join(__dirname, 'dist'),
+      ],
     },
-    compress: true,
-    port: 9000,
-    hot: true
-  }
+    plugins: [
+      new MiniCssExtractPlugin({
+        filename: '[name].css',
+      }),
+      new HtmlWebpackPlugin({
+        template: './src/index.html',
+        filename: 'index.html',
+        inject: 'body',
+        // Only minify HTML in production mode
+        minify: isReadableOutput ? false : {
+          collapseWhitespace: true,
+          removeComments: true,
+          removeRedundantAttributes: true,
+          removeScriptTypeAttributes: true,
+          removeStyleLinkTypeAttributes: true,
+          useShortDoctype: true
+        }
+      })
+    ],
+    // Control optimization based on readable output flag
+    optimization: {
+      minimize: !isReadableOutput
+    },
+    devServer: {
+      static: {
+        directory: path.join(__dirname, 'dist'),
+      },
+      compress: true,
+      port: 9000,
+      hot: true
+    }
+  };
 };
